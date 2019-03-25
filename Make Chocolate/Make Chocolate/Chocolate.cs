d8f5a05a5843e39b32f9953b9b5d6694eb992c36
FernandoSolano20 <FernandoSolano20@users.github.com>
2019-03-25 10:47:16 -0600

    First Commit of Exercise 3

---
+++ Make Chocolate/Make Chocolate/Chocolate.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Make_Chocolate
+{
+    public static class Chocolate
+    {
+        static public int MakeChocolate(int smallBars, int bigBars, int goal)
+        {
+            int cantBigBars = 0;
+            while(bigBars > 0 && cantBigBars <= bigBars)
+            {
+                if ((cantBigBars + 1) * 5 < goal)
+                {
+                    cantBigBars++;
+                }
+                bigBars--;
+            }
+
+            goal -= (cantBigBars * 5);
+
+            if(goal <= smallBars)
+            {
+                return goal;
+            }
+            return -1;
+        }
+    }
+}
---
+++ Make Chocolate/Make Chocolate/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Make_Chocolate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numberSmallBars = Chocolate.MakeChocolate(4, 1, 7);
+            Console.WriteLine(numberSmallBars);
+        }
+    }
+}
